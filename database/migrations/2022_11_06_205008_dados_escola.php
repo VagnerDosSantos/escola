a3bba5271ae5e1c24d7390e2df84c3f454caca3c
VagnerDosSantos <38004597+VagnerDosSantos@users.noreply.github.com>
2022-11-07 20:18:58 -0300

    criacao de migration de escolas

--- database/migrations/2022_11_06_205008_dados_escola.php
+++ database/migrations/2022_11_06_205008_dados_escola.php
@@ -12,7 +12,6 @@ return new class() extends Migration {
      */
     public function up()
     {
-        // Refazer as seeders de estados / municipios / distritos
         Schema::create('dados_escola', function (Blueprint $table) {
             $table->id();
             $table->foreignId('escola_id')->constrained('escolas');
@@ -21,6 +20,8 @@ return new class() extends Migration {
             $table->date('inicio_ano_letivo')->nullable();
             $table->date('termino_ano_letivo')->nullable();
             $table->string('cep', 10);
+            $table->tinyInteger('localizacao');
+            $table->tinyInteger('localizacao_diferenciada');
             $table->string('municipio_id', 10);
             $table->string('distrito_id', 20);
             $table->string('endereco', 100);
@@ -30,6 +31,22 @@ return new class() extends Migration {
             $table->string('telefone_1', 11)->nullable();
             $table->string('telefone_2', 11)->nullable();
             $table->string('email', 100)->nullable();
+            $table->tinyInteger('dependencia_administrativa');
+            $table->string('orgao_escola_publica_vinculada')->nullable();
+            $table->string('mantenedora_escola_privada')->nullable();
+            $table->string('cnpj_mantenedora_escola_privada', 14)->nullable();
+            $table->string('cnpj_escola_privada', 14)->nullable();
+            $table->string('poder_publico_responsavel_parceria_convenio')->nullable();
+            $table->string('formas_contratacao')->nullable();
+            $table->tinyInteger('regulamentacao_conselho')->default(0);
+            $table->string('esfera_administrativa_conselho')->nullable();
+            $table->tinyInteger('vinculo_educacao_basica_superior')->default(0);
+            $table->string('codigo_escola_sede', 8)->nullable();
+            $table->string('codigo_ies', 9)->nullable();
+
+            $table->string('created_by', 100);
+            $table->string('updated_by', 100)->nullable();
+            $table->string('deleted_by', 100)->nullable();
 
             $table->timestamps();
             $table->softDeletes();
@@ -46,5 +63,6 @@ return new class() extends Migration {
      */
     public function down()
     {
+        Schema::dropDatabaseIfExists('dados_escola');
     }
 };
